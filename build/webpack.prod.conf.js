427c90daa7850fb3670a6fccaadd6a78f0e960a7
Hua Meng <hmeng22@users.noreply.github.com>
2017-04-13 16:35:38 -0400

    Update webpack.prod.conf.js

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,6 +5,7 @@ var baseWebpackConfig = require('./webpack.base.conf');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(baseWebpackConfig, {
     output: {
@@ -43,6 +44,18 @@ module.exports = merge(baseWebpackConfig, {
         }),
         // extract webpack runtime and module manifest to its own file in order to
         // prevent vendor hash from being updated whenever app bundle is updated
-        new webpack.optimize.CommonsChunkPlugin({name: 'manifest', chunks: ['vendor']})
+        new webpack.optimize.CommonsChunkPlugin({name: 'manifest', chunks: ['vendor']}),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeAttributeQuotes: true
+            },
+            // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+            chunksSortMode: 'dependency'
+        })
     ]
 });
